14a05784835b133b2aa452e75e2745038ec171c8
Rawan Bazadough <bazadough1997@gmail.com>
2020-12-16 14:18:07 +0200

    MAinSearch component

---
+++ client/src/components/MainSearch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class MainSearch extends Component {
+    render() {
+        return (
+            <div>
+                Rawan
+            </div>
+        )
+    }
+}
+
+export default MainSearch;
