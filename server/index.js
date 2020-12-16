17f306306dd1956ef64342f5964109314841e871
Rawan Bazadough <bazadough1997@gmail.com>
2020-12-16 10:48:18 +0200

    files structure

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
-import cors from "cors";
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
@@ -9,15 +9,12 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL =
-  "mongodb+srv://gamecity:gamecity@gamecity.bw3we.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const CONNECTION_URL = 'mongodb+srv://gamecity:gamecity@gamecity.bw3we.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
 const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
-  )
-  .catch((error) => console.log(error.message));
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+   .then(()=> app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+   .catch((error)=> console.log(error.message) )
 
-mongoose.set("useFindAndModify", false);
+mongoose.set('useFindAndModify', false)
