f22ff69a4a43398b1f2e85057737d0245a4a3749
abeer-rawash <abeer.rawa96@gmail.com>
2020-12-16 10:16:37 +0200

    setup ready

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>
+                HI FROM RAHAF AND ABEER
+            </h1>
+        </div>
+    )
+}
+
+export default App;
