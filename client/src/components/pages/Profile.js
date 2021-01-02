6955c84ab751736bc8258b3ce5a0eca499946062
Rawan Bazadough <bazadough1997@gmail.com>
2021-01-02 09:50:58 +0200

    fixing profile img

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -3,52 +3,50 @@ import { connect } from "react-redux";
 import { fetchUser } from "../../actions/index";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
-import Profilegames from "../games/Profilegames";
+import Profilegames from "../games/Profilegames"
 //action  setGames
 //set state
 //map //if statmemnt
 //const[]
 function Profile({ userData, fetchUser }) {
-  let { email } = useParams();
-  console.log(email);
+  let {email}= useParams();
+  console.log(email)
   useEffect(() => {
     fetchUser(email);
-  }, [email, fetchUser]);
-
-  return userData.loading ? (
-    <h3>Loading</h3>
-  ) : userData.error ? (
-    <h3>{userData.error}</h3>
-  ) : (
-    <div>
-      <h3>{window.localStorage.username}'s info:</h3>
+  },[email,fetchUser]);
+  return userData.loading ? (<h2>Loading</h2>) : userData.error ? (<h2>{userData.error}</h2>) : (<div><h2>User Information</h2>
       <div>
         <hr></hr>
-        <img src={userData.user.url} alt="profile_pic" width="150px"></img>
-        <Link
-          to={{ pathname: `/editProfile/${userData.user.email}` }}
-          className="btn btn-info float-right"
-        >
-          Edit
-        </Link>
         {userData && userData.user && (
-          <div>
-            <h6>Username: {userData.user.username}</h6>
-            <h6>
-              Name: {userData.user.firstName} {userData.user.lastName}
-            </h6>
-            <h6>Email: {userData.user.email}</h6>
-            <h6>City: Jordan/{userData.user.city}</h6>
-            <h6>Phone number: {userData.user.phoneNo}</h6>
-            <h6>Birthday: {userData.user.birthday}</h6>
-          </div>
+          <ul>
+          -UserName:
+          <li>{userData.user.username}</li>
+          -FirstName:
+          <li>{userData.user.firstName}</li>
+          -LastName:
+          <li>{userData.user.lastName}</li>
+          -Email:
+          <li>{userData.user.email}</li>
+          -City:
+          <li>{userData.user.city}</li>
+          -PhoneNo:
+          <li>{userData.user.phoneNo}</li>
+          -Birthday:
+          <li>{userData.user.birthday}</li>
+          </ul>    
         )}
+       <img src={userData.user.url} alt ="profile_pic" width="150px"></img>
+                   <Link
+              to={{ pathname: `/editProfile/${userData.user.email}` }}
+              className="btn btn-info"
+            >
+              Edit
+            </Link>
       </div>
       <Profilegames />
     </div>
   );
 }
-
 const mapStateToProps = (state) => {
   return {
     userData: state.user,
@@ -56,11 +54,9 @@ const mapStateToProps = (state) => {
     id: window.localStorage.id,
   };
 };
-
 const mapDispatchToProps = (dispatch) => {
   return {
     fetchUser: (email) => dispatch(fetchUser(email)),
   };
 };
-
 export default connect(mapStateToProps, mapDispatchToProps)(Profile);
